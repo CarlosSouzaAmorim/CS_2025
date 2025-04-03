e318a2aa3ac972d24933015fb6eb58ec45352928
Carlos Amorim <30291264+CarlosSouzaAmorim@users.noreply.github.com>
2025-04-03 10:31:28 -0300

    Update ks.h

--- ks.h
+++ ks.h
@@ -13,11 +13,24 @@ typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
 typedef unsigned long long int int64;
+typedef unsigned _BitInt(128) int128;
 
-#define $8 (int8 *)
-#define $6 (int16)
-#define $2 (int32)
-#define $4 (int64)
+typedef int32 word;    //AES int32 is a word
+typedef int128 key;    //AES key
+typedef word wordarray[44];
+
+// key schedule --> create subkeys
+// actual encryption and decr
+
+//  128 kit key ---> 11 subkeys of the same length
+// in 44 keys
+
+
+#define $1 (int8 *)
+#define $2 (int16)
+#define $4 (int32)
+#define $8 (int64)
+#define $16 (int128)
 #define $c (char *)
 #define $i (int)
 
