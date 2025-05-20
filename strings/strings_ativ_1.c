f1953beda11d2b556a9c27e2f521b393bfa1d25b
Carlos Amorim <30291264+CarlosSouzaAmorim@users.noreply.github.com>
2025-05-20 03:36:44 +0000

    VSCODE fixed

--- strings/strings_ativ_1.c
+++ strings/strings_ativ_1.c
@@ -1,3 +1,40 @@
+#include <stdio.h>
+#include "strings_ativ_1.h"
+
+#define LEN_DEF 30
+//strings_ativ_1_test.c
+
+
+int strlen(char *s); /* String Length */
+
+int isnull(char *s); /* Not included */
+int isnull2(char *s);
+
+char *strcpy(char *dest, char *orig);
+char *strcpy2(char *dest, char *orig);
+
+char *strcat(char *dest, char *orig); /* String Concat */
+char *strcat2(char *dest, char *orig);
+char *strcat3(char *dest, char *orig);
+
+
+
+int main(int argc, char* argv[]){
+    char stringizinha[LEN_DEF] = {'o', 'i'};    
+    printf("O tamanho da string é: %d", strlen(stringizinha));
+    
+    // Testes
+    printf("\n\nTestes:\n");
+    printf("strlen(\"oi\") = %d\n", strlen("oi"));
+    printf("strlen(\"\") = %d\n", strlen(""));
+    printf("isnull(\"oi\") = %d\n", isnull("oi"));
+    printf("isnull(\"\") = %d\n", isnull(""));
+    printf("strcpy(stringizinha, \"oi\") = %s\n", strcpy(stringizinha, "oi"));
+    printf("strcat(stringizinha, \" mundo!\") = %s\n", strcat(stringizinha, " mundo!"));
+    
+    return 0;
+}
+
 
 // 1) Devolve o número de caracteres existentes na string (sem '\0')
 
